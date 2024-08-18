6ebaa5299ec55e93a806b8e677e61050ff871e5e
Funny-guuy <d.ntambakwa@gmail.com>
2024-08-18 19:58:41 +0000

    hello 434353

--- lessons/00_Turtles/03b_Turtle_Tricks.py
+++ lessons/00_Turtles/03b_Turtle_Tricks.py
@@ -8,16 +8,20 @@ to see how to use the turtle commands.
 
 """
 
+sides = input()
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600)    # Set the size of the window
+angle = (360 / int(sides))
 tina = turtle.Turtle()                  # Create a turtle named tina
-
-# Use tina.forward() and tina.left() to draw a pentagon
-# Make each side of the pentagon a different color with 
+tina.speed(int(sides))
+for i in range(int(sides)):
+    tina.forward(90)
+    tina.left(angle)
+# Make each side of the triangle a different color with 
 # tina.pencolor()
-
-
+    tina.pencolor("green")
 ... # Your code here
+
 
 turtle.exitonclick()                    # Close the window when we click on it
--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -9,3 +9,13 @@ write a new program that uses a loop to draw a pentagon.
 """
 
 ... # Your code here
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup (width=600, height=600)    # Set the size of the window
+sides = int(input())
+angle = (360 / sides)
+tina = turtle.Turtle()                  # Create a turtle named tina
+for i in range(sides):
+    tina.forward(150)
+    tina.left(angle)
+
+turtle.exitonclick()                    # Close the window when we click on it
